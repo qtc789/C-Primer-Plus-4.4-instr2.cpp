12d3aee6bed327ae1395a524d7a21735640aafd2
qtc789 <qiantongci@126.com>
2024-11-14 23:49:52 +0800

    Add files via upload

---
+++ C++ Primer Plus 4.4 instr2.cpp/C++ Primer Plus 4.4 instr2.cpp.cpp
@@ -0,0 +1,21 @@
+﻿// C++ Primer Plus 4.4 instr2.cpp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name;\n";
+    cin.getline(name,ArSize);
+    cout << "Enter your favorite dessert;\n";
+    cin.getline(dessert,ArSize);
+    cout << "I have some delicious " << dessert;
+    cout << " for you," << name << " .\n";
+
+    return 0;
+}
